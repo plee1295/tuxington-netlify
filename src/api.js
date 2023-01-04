5acc4ea50945a34c88468cd530ca68dbe0c58c3d
Parker Lee <parker.lee.1295@gmail.com>
2023-01-03 20:11:03 -0600

    first commit

---
+++ src/api.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const serverless = require('serverless-http')
+
+const app = express()
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json({'hello': 'world'})
+})
+
+router.post('/', (req, res) => {
+  res.json({'challenge': req.body.challenge})
+})
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app)
